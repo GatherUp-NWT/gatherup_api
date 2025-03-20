56b7250c11a0b0d568c1e43363cadd76e8c504c7
NejraStrsevic <strsevicnejra@gmail.com>
2025-03-20 14:45:21 +0100

    Fixed Auth service

---
+++ AuthService/src/main/java/org/app/authservice/respository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.app.authservice.respository;
+
+import org.app.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
