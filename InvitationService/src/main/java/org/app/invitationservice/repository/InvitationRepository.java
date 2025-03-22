f9494dcfa7b0bb2db8884e254b8d4b7e9bb1ee65
NejraStrsevic <strsevicnejra@gmail.com>
2025-03-22 14:26:20 +0100

    Fixed Invitation

---
+++ InvitationService/src/main/java/org/app/invitationservice/repository/InvitationRepository.java
@@ -0,0 +1,9 @@
+package org.app.invitationservice.repository;
+
+import org.app.invitationservice.entity.Invitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
+}
