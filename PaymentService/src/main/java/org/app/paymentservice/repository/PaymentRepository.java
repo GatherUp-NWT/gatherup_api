a075070a71cb46bb400b1794199c7c8f4c556d03
NejraStrsevic <strsevicnejra@gmail.com>
2025-03-23 16:04:29 +0100

    Payment controllers, models, dtos, error handling

--- PaymentService/src/main/java/org/app/paymentservice/repository/PaymentRepository.java
+++ PaymentService/src/main/java/org/app/paymentservice/repository/PaymentRepository.java
@@ -1,7 +1,14 @@
 package org.app.paymentservice.repository;
 
+import java.util.List;
+import java.util.UUID;
 import org.app.paymentservice.entity.Ticket;
+import org.app.paymentservice.response.PaymentModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PaymentRepository extends CrudRepository<Ticket, Long> {
+
+
+  List<Ticket> findByUserId(UUID userId);
 }
