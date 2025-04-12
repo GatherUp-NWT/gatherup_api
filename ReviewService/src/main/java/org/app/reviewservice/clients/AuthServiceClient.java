670d6c23af7bdff059993391f72fb3f285e66e3e
asuta2 <asuta2@etf.unsa.ba>
2025-04-13 01:27:22 +0200

    Refactor application properties for services to use config server and set active profile to dev

--- ReviewService/src/main/java/org/app/reviewservice/clients/AuthServiceClient.java
+++ ReviewService/src/main/java/org/app/reviewservice/clients/AuthServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "AuthService")
+@FeignClient(name = "auth-service")
 public interface AuthServiceClient {
     @GetMapping("/api/v1/users/{id}")
     UserDTO getUserById(@PathVariable String id);
--- ReviewService/src/main/java/org/app/reviewservice/clients/EventServiceClient.java
+++ ReviewService/src/main/java/org/app/reviewservice/clients/EventServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "EventService")
+@FeignClient(name = "event-service")
 public interface EventServiceClient {
     @GetMapping("/events/{eventId}")
     EventResponseDTO getEventById(@PathVariable String eventId);
