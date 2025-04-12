1ec8e83bc35f70eb61a5440985d140d508fca076
NejraStrsevic <strsevicnejra@gmail.com>
2025-04-12 11:37:40 +0200

    Time Status

--- RegistrationService/src/main/java/org/app/registrationservice/mapper/RegistrationMapper.java
+++ RegistrationService/src/main/java/org/app/registrationservice/mapper/RegistrationMapper.java
@@ -3,7 +3,9 @@ package org.app.registrationservice.mapper;
 import org.app.registrationservice.dto.RegistrationDTO;
 import org.app.registrationservice.entity.Registration;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 
+@MapperConfig
 @Mapper(componentModel = "spring")
 public interface RegistrationMapper {
     RegistrationDTO toDto(Registration registration);
